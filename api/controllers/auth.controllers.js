8a37cf724a551cc9a54a7b99304c8b48957ce160
Yash Parmar <yash20545@gmail.com>
2024-07-07 02:07:06 +0530

    complete sign in page functionality

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import User from "../models/user.models.js";
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import {errorHandler } from '../utils/error.js';
 export const signup = async(req,res,next) =>{
      const { username , email , password } = req.body ;
      const hashedPassword = bcryptjs.hashSync(password,10);
