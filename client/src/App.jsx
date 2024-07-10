e872386e624b352ec166a64e450b3326189e3a79
Yash Parmar <yash20545@gmail.com>
2024-07-10 18:35:39 +0530

    complete profile page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,25 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import About from './pages/About'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Header from './Components/Header'
-import PrivateRoute from './Components/PrivateRoute'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './Components/Header';
+import PrivateRoute from './Components/PrivateRoute';
 
 export default function App() {
-  return <BrowserRouter>
-     <Header/>
-     <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/sign-in' element={<SignIn/>} />
-        <Route path='/sign-up' element={<SignUp/>} />
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
         </Route>
-     </Routes>
-
-  </BrowserRouter>
+      </Routes>
+    </BrowserRouter>
+  );
 }
